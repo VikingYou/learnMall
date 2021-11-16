66acb8e0cea5224287a11185e435b688f4fbcf8d
bafeimao <ycd200709@163.com>
2021-11-16 21:47:38 +0800

    initial commit

---
+++ src/test/java/com/mall/learnMall/LearnMallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mall.learnMall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnMallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
